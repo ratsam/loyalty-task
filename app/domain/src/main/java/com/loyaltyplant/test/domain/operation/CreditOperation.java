f85740c10ae42d95b2f5f3d432bec3190c4eca4a
ratsam <m4spam@gmail.com>
2014-09-12 13:12:17 +0400

    Better naming.

--- app/domain/src/main/java/com/loyaltyplant/test/domain/operation/CreditOperation.java
+++ app/domain/src/main/java/com/loyaltyplant/test/domain/operation/CreditOperation.java
@@ -10,22 +10,22 @@ import javax.annotation.Nonnull;
 import java.math.BigDecimal;
 
 /**
- * Deposit Operation.
- * Perform account replenishment for given amount.
+ * Credit Operation.
+ * Perform balance replenishment for given amount.
  * Amount must have non-negative value.
  *
  * @author Maksim Zakharov
  * @since 1.0
  */
-public class DepositOperation implements BalanceOperation {
+public class CreditOperation implements BalanceOperation {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DepositOperation.class);
+    private static final Logger LOG = LoggerFactory.getLogger(CreditOperation.class);
 
     private final BigDecimal amount;
 
-    public DepositOperation(@Nonnegative BigDecimal amount) {
+    public CreditOperation(@Nonnegative BigDecimal amount) {
         if (amount.compareTo(new BigDecimal("0")) < 0) {
-            throw new IllegalArgumentException("Can't deposit negative amount");
+            throw new IllegalArgumentException("Can't credit negative amount");
         }
 
         this.amount = amount;
@@ -34,7 +34,7 @@ public class DepositOperation implements BalanceOperation {
     @Override
     public void apply(@Nonnull Balance balance) {
         if (LOG.isInfoEnabled()) {
-            LOG.info("Applying deposit of {} for Balance #{}", getAmount(), balance.getId());
+            LOG.info("Applying credit of {} for Balance #{}", getAmount(), balance.getId());
         }
         doApply(balance);
     }
--- app/domain/src/test/java/com/loyaltyplant/test/domain/operation/CreditOperationTest.java
+++ app/domain/src/test/java/com/loyaltyplant/test/domain/operation/CreditOperationTest.java
@@ -11,29 +11,29 @@ import static org.junit.Assert.*;
  * @author Maksim Zakharov
  * @since 1.0
  */
-public class DepositOperationTest {
+public class CreditOperationTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testConstructionWithNegativeAmount() {
-        new DepositOperation(new BigDecimal("-0.1"));
+        new CreditOperation(new BigDecimal("-0.1"));
     }
 
     @Test
     public void testConstructionWithZeroAmount() {
         // I don't know why, but why not?
-        new DepositOperation(new BigDecimal("0"));
+        new CreditOperation(new BigDecimal("0"));
     }
 
     @Test
     public void testConstructWithPositiveAmount() {
-        new DepositOperation(new BigDecimal("1"));
+        new CreditOperation(new BigDecimal("1"));
     }
 
     @Test
     public void testApplyZeroDeposit() {
         final Balance balance = new Balance(1, new BigDecimal("0.1"));
 
-        new DepositOperation(new BigDecimal("0")).apply(balance);
+        new CreditOperation(new BigDecimal("0")).apply(balance);
 
         assertEquals(new BigDecimal("0.1"), balance.getAmount());
     }
@@ -42,7 +42,7 @@ public class DepositOperationTest {
     public void testApplyPositiveDeposit() {
         final Balance balance = new Balance(1, new BigDecimal("0"));
 
-        new DepositOperation(new BigDecimal("42")).apply(balance);
+        new CreditOperation(new BigDecimal("42")).apply(balance);
 
         assertEquals(new BigDecimal("42"), balance.getAmount());
     }
