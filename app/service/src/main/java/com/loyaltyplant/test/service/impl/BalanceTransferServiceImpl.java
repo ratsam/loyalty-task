1ac7fd4f6ecb5d980bc6ff8326f9012ebec0eec6
ratsam <m4spam@gmail.com>
2014-09-21 18:16:50 +0400

    BalanceTransferServiceImpl implementation.

--- app/service/src/main/java/com/loyaltyplant/test/service/impl/BalanceTransferServiceImpl.java
+++ app/service/src/main/java/com/loyaltyplant/test/service/impl/BalanceTransferServiceImpl.java
@@ -1,32 +1,54 @@
 package com.loyaltyplant.test.service.impl;
 
+import com.loyaltyplant.test.domain.operation.CreditOperation;
+import com.loyaltyplant.test.domain.operation.DebitOperation;
 import com.loyaltyplant.test.service.BalanceTransferService;
+import com.loyaltyplant.test.service.OperationApplier;
+import com.loyaltyplant.test.service.OperationApplyingInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 /**
+ * Balance transfer service.
+ * Handles Balance value modification in a safe manner, automatically retrying if
+ * {@link org.springframework.dao.OptimisticLockingFailureException} occurs.
+ *
  * @author Maksim Zakharov
  * @since 1.0
  */
+@Service("balanceTransferService")
 public class BalanceTransferServiceImpl implements BalanceTransferService {
+
+    private OperationApplier operationApplier;
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public void transfer(Integer fromBalance, Integer toBalance, BigDecimal amount) {
-        throw new UnsupportedOperationException();
+        operationApplier.applyOperations(Arrays.asList(
+                new OperationApplyingInfo(fromBalance, new DebitOperation(amount)),
+                new OperationApplyingInfo(toBalance, new CreditOperation(amount))
+        ));
     }
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
-    public BigDecimal credit(Integer balanceId, BigDecimal amount) {
-        throw new UnsupportedOperationException();
+    public void credit(Integer balanceId, BigDecimal amount) {
+        operationApplier.applyOperation(balanceId, new CreditOperation(amount));
     }
 
     @Override
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
-    public BigDecimal debit(Integer balanceId, BigDecimal amount) {
-        throw new UnsupportedOperationException();
+    public void debit(Integer balanceId, BigDecimal amount) {
+        operationApplier.applyOperation(balanceId, new DebitOperation(amount));
+    }
+
+    @Autowired
+    public void setOperationApplier(OperationApplier operationApplier) {
+        this.operationApplier = operationApplier;
     }
 }
---
+++ app/service/src/test/java/com/loyaltyplant/test/service/impl/BalanceTransferServiceImplTest.java
@@ -0,0 +1,58 @@
+package com.loyaltyplant.test.service.impl;
+
+import com.loyaltyplant.test.domain.operation.AbstractOperation;
+import com.loyaltyplant.test.service.BalanceTransferService;
+import com.loyaltyplant.test.service.OperationApplier;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Maksim Zakharov
+ * @since 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BalanceTransferServiceImplTest.Config.class)
+public class BalanceTransferServiceImplTest {
+
+    @Autowired
+    private BalanceTransferService balanceTransferService;
+
+    @Autowired
+    private OperationApplier operationApplierMock;
+
+    @Before
+    public void setUp() {
+        reset(operationApplierMock);
+    }
+
+    @Test
+    public void testDebitSuccess() {
+        balanceTransferService.debit(1, new BigDecimal("10"));
+
+        verify(operationApplierMock).applyOperation(eq(Integer.valueOf(1)), any(AbstractOperation.class));
+        verifyNoMoreInteractions(operationApplierMock);
+    }
+
+    public static class Config {
+
+        @Bean
+        public OperationApplier operationApplierMock() {
+            return mock(OperationApplier.class);
+        }
+
+        @Bean(autowire = Autowire.BY_TYPE)
+        public BalanceTransferService balanceTransferService() {
+            return new BalanceTransferServiceImpl();
+        }
+    }
+}
